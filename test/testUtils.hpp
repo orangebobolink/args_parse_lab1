6f85f70eeedd4c218437e55f24637cd35ed63238
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-04 15:53:33 +0300

    update

--- test/testUtils.hpp
+++ test/testUtils.hpp
@@ -11,19 +11,19 @@
 namespace test_utils
 {
 	/**
-		* \brief Получем настроенный парсер.
+		* \brief пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	args_parse::Parser getParser(const int argc, const char** argv);
 	/**
-		* \brief Получем тестовый набор аргументов.
+		* \brief пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	std::vector< std::unique_ptr<args::Arg>> getTestArgs();
 	/**
-		* \brief Создаем тестовый случай.
+		* \brief пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	std::pair<int, std::unique_ptr<const char* []>> createTestCase(const std::vector<const char*>& strings);
 	/**
-		* \brief Создаем набор тестовых случаев.
+		* \brief пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	*/
 	std::vector<std::pair<int, std::unique_ptr<const char* []>>> createTestCases(const std::vector<std::vector<const char*>>& testCasesStrings);
 }
