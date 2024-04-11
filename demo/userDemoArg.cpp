3a481063956b30bff27162448f2ef3ca8b6425c8
Nikita Tishkov <nikitatishkou@gmail.com>
2024-04-11 09:53:50 +0300

    update validator

--- demo/userDemoArg.cpp
+++ demo/userDemoArg.cpp
@@ -1,6 +1,9 @@
 #include "userDemoArg.hpp"
+#include <regex>
 
 types::Result<bool> IpValidator::validate(std::string& value)
 {
-	return {true};
+	const static  std::regex pattern("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+
+	return { std::regex_match(value, pattern) };
 }
